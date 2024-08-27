b32164ec6489394ef795821159d609be52d536aa
JaviGarcia9814 <35715692+JaviGarcia9814@users.noreply.github.com>
2024-08-26 17:27:19 -0700

    Add files via upload

---
+++ myMain.kt
@@ -0,0 +1,14 @@
+//this program will make and array will loop the array and print it out
+fun main(args: Array<String>)
+{
+    //array of animals
+    val animals = arrayOf("dog","Cat","Bird")
+
+    //loop that will print out the array
+    for( i in animals)
+    {
+    //having trouble printing on new line 
+     print(i+'\n')
+    }
+
+}
